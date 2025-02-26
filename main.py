8b2f4669f3139ba36805524a8f964f1b6ac82c4a
bbtqqrl <makspeys@gmail.com>
2025-02-26 13:20:50 +0100

    modified:   main.py modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import asyncio
-import os
-import threading
-from flask import Flask
-from aiogram import Bot, Dispatcher
+from aiogram import Router
+from aiogram import F, Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-from callbacks import callback_handlers
-from handlers import bot_message, user_commands
-
-
-# Токен бота
-TOKEN = '7213514246:AAFrt2iq7MkFh-d3PKhSrjDoAok5X_1AsBc'
-
-# Ініціалізація бота і диспетчера
-bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+bot = Bot('7213514246:AAFrt2iq7MkFh-d3PKhSrjDoAok5X_1AsBc', default=DefaultBotProperties(parse_mode='HTML'))
 dp = Dispatcher()
+from handlers import bot_message, user_commands
+from callbacks import callback_handlers
 
-# Функція для запуску бота у фоні
-async def run_bot():
+async def main():
     dp.include_routers(
         user_commands.router,
         bot_message.router,
@@ -25,19 +16,6 @@ async def run_bot():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-def start_bot():
-    asyncio.run(run_bot())
 
-# Запуск бота у окремому потоці
-threading.Thread(target=start_bot).start()
-
-# Flask-сервер для Render
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is running!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Використовуємо порт Render
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    asyncio.run(main())
